15e65c6867177ec22171b519d719d28b863d1659
12999529 <12999529@itisrossi.vi.it>
2025-11-12 11:37:29 +0100

    .

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void) {
     int n = 0;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        fork();
-        printf("PID numero %d %d\n",i, getpid());
+    for (int i = 1; i < n + 1 ; i++) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            printf("fork failed\n");
+        }
+        if (pid == 0) {
+            printf("child: %d, numero %d \n",i, getpid());
+        } else {
+            //printf("parent %d, numero: %d \n",i, getppid());
+            wait(0);
+            break;
+        }
     }
     return 0;
 }
